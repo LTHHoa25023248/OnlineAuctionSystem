09140368accd4eb0c57f5ae00d5cf783ff8cefaf
Pham Duc Huy <25023270@vnu.edu.vn>
2026-04-16 08:38:37 +0700

    khởi tạo user, bidder, seller, admin trong base-model

--- src/main/java/com/example/auctionmanagementsystem/model/Seller.java
+++ src/main/java/com/example/auctionmanagementsystem/model/Seller.java
@@ -1,7 +1,16 @@
 package com.example.auctionmanagementsystem.model;
 
-public class Seller extends User{
-    public Seller(String id, String username, String password){
-        super(id,username, password);
+public class Seller extends User {
+    public Seller(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public String getRoleName() {
+        return "SELLER";
+    }
+
+    public void createItem() {
+        System.out.println(username + " đang tạo sản phẩm đấu giá mới...");
     }
 }
