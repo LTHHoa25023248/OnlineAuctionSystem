f85606c10f7ecc7127c547274296c39e6e767ea2
HONG DIEP <nguyenmaihongdiep@gmail.com>
2026-04-18 10:22:39 +0700

    feat: implement item subclasses

---
+++ src/main/java/com/example/auctionmanagementsystem/model/Art.java
@@ -0,0 +1,19 @@
+package com.example.auctionmanagementsystem.model;
+
+import java.time.LocalDateTime;
+
+public class Art extends Item {
+    private String artist, theme, material;
+
+    public Art(String id, String name, String description, double startingPrice, LocalDateTime startTime, LocalDateTime endTime, String artist, String theme, String material) {
+        super(id, name, description, startingPrice, startTime, endTime);
+        this.artist =  artist;
+        this.theme = theme;
+        this.material = material;
+    }
+
+    @Override
+    public String getCategoryDetails() {
+        return String.format("Artist: %d | Theme: %s | Material: %n", artist, theme, material);
+    }
+}
