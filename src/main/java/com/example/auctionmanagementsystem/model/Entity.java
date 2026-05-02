f9bc2d1ccfc240667f60ef048f5de5d5357ca8f0
Phạm Đức Huy <25023270@vnu.edu.vn>
2026-05-02 23:25:50 +0700

    Refactor Entity class ID initialization
    Removed automatic ID generation and initialized ID to 0.

--- src/main/java/com/example/auctionmanagementsystem/model/Entity.java
+++ src/main/java/com/example/auctionmanagementsystem/model/Entity.java
@@ -1,13 +1,10 @@
 package com.example.auctionmanagementsystem.model;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public abstract class Entity {
-    private static final AtomicInteger AUTO_ID = new AtomicInteger(1);
     protected int id;
 
     public Entity() {
-        this.id = AUTO_ID.getAndIncrement();
+        this.id = 0;
     }
 
     public int getId() {
