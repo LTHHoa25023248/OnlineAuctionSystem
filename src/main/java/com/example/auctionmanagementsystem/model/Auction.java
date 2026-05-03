39a1de4867b49b167b6f90ebcce9de692bde5f49
HongHoa <25023248@vnu.edu.vn>
2026-05-03 10:02:43 +0700

    add setter

--- src/main/java/com/example/auctionmanagementsystem/model/Auction.java
+++ src/main/java/com/example/auctionmanagementsystem/model/Auction.java
@@ -14,7 +14,8 @@ public class Auction {
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private List<BidTransaction> historyBid=new ArrayList<>();
-
+    private List<AutoBid> hisAutoBid=new ArrayList<>();
+    private final ReentrantLock lock=new ReentrantLock();
     public double getCurrentPrice(){
         return currentPrice;
     }
@@ -34,4 +35,41 @@ public class Auction {
     public Item getItem(){
         return item;
     }
+    public List<AutoBid> getHisAutoBid9(){
+        return hisAutoBid;
+    }
+    public ReentrantLock getLock() {
+        return lock;
+    }
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public void setHighestBidder(User highestBidder) {
+        this.highestBidder = highestBidder;
+    }
+
+    public void setStatus(AuctionStatus status) {
+        this.status = status;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+    public boolean isOpen() {
+        return status == AuctionStatus.RUNNING;
+    }
+
+    public void addBid(BidTransaction bid) {
+        historyBid.add(bid);
+    }
+
+    public void addAutoBid(AutoBid autoBid) {
+        hisAutoBid.add(autoBid);
+    }
+
 }
