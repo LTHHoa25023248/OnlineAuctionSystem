eb5ec27a1633e39cdf487f942a83c0f286dd518b
HongHoa <25023248@vnu.edu.vn>
2026-05-13 23:32:41 +0700

    hoan thien ItemDao

--- src/main/java/com/example/auctionmanagementsystem/dao/ItemDAO.java
+++ src/main/java/com/example/auctionmanagementsystem/dao/ItemDAO.java
@@ -10,186 +10,157 @@ import java.sql.*;
 
 import static java.lang.Integer.parseInt;
 
- public class ItemDAO implements DAOInterface <Item>{
-    public int insert(Item item){
-        //Su dung commit, rollback. Commit de luu du lieu neu da insert thanh cong, neu khong thi rollback de tra ve trang thai ban dau luc chua insert
-        //tranh truwowng hop bang item co du lieu nhung bang vehicle,... ko co su lieu
+ public class ItemDAO implements DAOInterface <Item> {
+     public int insert(Item item) {
+         //Su dung commit, rollback. Commit de luu du lieu neu da insert thanh cong, neu khong thi rollback de tra ve trang thai ban dau luc chua insert
+         //tranh truong hop bang item co du lieu nhung bang vehicle,... ko co du lieu
 
-        Connection connect = null;
-        try{
-                 connect= DatabaseConnection.getConnection();
-                 //luu du lieu tam thoi, ko luu vao database
-                 connect.setAutoCommit(false);
-            String sqlItem="INSERT INTO items( name, description, starting_price, item_type) VALUES(?,?,?,?)";
+         Connection connect = null;
+         try {
+             connect = new DatabaseConnection().getConnection();
+             //luu du lieu tam thoi, ko luu vao database
+             //tat auto commit, dam bao insert cha+con thanh cong hoac that bai cung luc
+             connect.setAutoCommit(false);
+             String sqlItem = "INSERT INTO items( name, description, starting_price, item_type) VALUES(?,?,?,?)";
 
-            //Bang luu du lieu
-            PreparedStatement psItem=connect.prepareStatement(sqlItem, Statement.RETURN_GENERATED_KEYS);
-            psItem.setString(1,item.getName());
-            psItem.setString(2,item.getDescription());
-            psItem.setDouble(3,item.getStartingPrice());
-            //xac dinh loai item
-              String type = "";
-              if (item instanceof Vehicle vehicle){
-                  type="VEHICLE";
-              }else if (item instanceof Art){
-                  type="ART";
-              }else if (item instanceof Electronics){
-                  type="ELECTRONICS";
-              }
-              //luu item vao DataBase
-              psItem.setString(4,type);
-              psItem.executeUpdate();
-              //lay ID tu dong sinh ra ru DB
-              ResultSet  resultSet=psItem.getGeneratedKeys();
-              int itemId=-1;
-              if(resultSet.next()){
-                  itemId=resultSet.getInt(1);
-                  item.setId(String.valueOf(itemId));
-              }
-              //ko lay duoc id thi loi
-            if(itemId==-1){
-                throw new RuntimeException("Cannot get generated item ID")
-            }
+             //Bang luu du lieu
+             PreparedStatement ps = connect.prepareStatement(sqlItem, Statement.RETURN_GENERATED_KEYS);
+             ps.setString(1, item.getName());
+             ps.setString(2, item.getDescription());
+             ps.setDouble(3, item.getStartingPrice());
+             ps.setString(4, item.getClass().getSimpleName().toUpperCase());
+             //Thuc thi insert
+             ps.executeUpdate();
+             //lay id tu tang tu database
+             ResultSet rs = ps.getGeneratedKeys();
+             int itemId;
+             if (rs.next()) {
+                 itemId = rs.getInt(1);
+                 item.setId(itemId);
+             } else {
+                 throw new RuntimeException("Cannot get generated ID");
+             }
+             //Insert bang con
+             //item tu biet no thuoc loai nao va tu insert
+             item.insertSubData(connect, itemId);
+             // commit du lieu, luu vao data
+             connect.commit();
+             return itemId;
+         } catch (Exception e) {
+             //nêú có lỗi xảy ra-> rolllback toan bo, neu luu du lieu sai thi tro ve trang thai ban dau
+             try {
+                 if (connect != null)
+                     connect.rollback();
+             } catch (Exception ex) {
+                 ex.printStackTrace();
+             }
+             throw new RuntimeException("Insert item failed");
+         } finally {
+             // tra ve trang thai tu dong luu du lieu xuong database
+             try {
+                 if (connect != null)
+                     connect.setAutoCommit(true);
 
-              if(item instanceof Vehicle vehicle){
-                //Thong tin rieng cua vehicle
-                String sqlVehicle="INSERT INTO vehicle_items(item_id, year, mileage) VALUES (?,?,?)";
-                PreparedStatement ps= connect.prepareStatement(sqlVehicle);
-                ps.setInt(1, itemId);
-                ps.setInt(2, vehicle.getYear());
-                ps.setDouble(3, vehicle.getMileage());
-                ps.executeUpdate();
+             } catch (Exception e) {
+                 e.printStackTrace();
+             }
+         }
 
-            }
-            else if( item instanceof Art art){
-                String sqlArt="INSERT INTO art_items(item_id, artist, theme, material) VALUES(?,?,?,?)";
-                PreparedStatement ps=connect.prepareStatement(sqlArt);
-                ps.setInt(1, itemId);
-                ps.setString(2, art.getArtist());
-                ps.setString(3, art.getTheme());
-                ps.setString(4,art.getMaterial());
-                ps.executeUpdate();
-            }
-            else if(item instanceof Electronics electronic){
-                String sqlElectronic="INSERT INTO electronics_items(item_id, brand, warranty_months) VALUES (?,?,?)";
-                PreparedStatement ps=connect.prepareStatement(sqlElectronic);
-                ps.setInt(1, itemId);
-                ps.setString(2, electronic.getBrand());
-                ps.setInt(3, electronic.getWarrantyMonths());
-                ps.executeUpdate();
-            }
-            //luu het du lieu neu tat ca ok
-            connect.commit();
-            return 1;//thanh cong
-        }catch (Exception e){
-            //rollback neu loi. Tro lai trang thai du lieu ban dau, huy toan bo thay doi
+     }
+
+     @Override
+     public int update(final Item item) {
+         Connection connect=null;
+         try {
+             connect = new DatabaseConnection().getConnection();
+             connect.setAutoCommit(false);
+             //update bang cha
+             String sql = "UPDATE items SET name=?, description =? WHERE id=?";
+             PreparedStatement ps = connect.prepareStatement(sql);
+             ps.setString(1, item.getName());
+             ps.setString(2, item.getDescription());
+             ps.setInt(3, item.getId());
+             //so dong bi anh huong
+              int affectedRows =ps.executeUpdate();
+             //update bang con
+             item.updateSubData(connect);
+             connect.commit();
+             return affectedRows;
+         } catch (Exception e) {
             try{
-                if(connect != null){
+                if (connect!= null)
                     connect.rollback();
-                }
-            }catch(SQLException sqle){
-                sqle.printStackTrace();
+            }catch(Exception ex){
+                ex.printStackTrace();
             }
-            e.printStackTrace();
-        }finally {
-            try{
-                if (connect != null )
-                    //tra ve trang thai tu dong luu du lieu xuong database
-                    //ko thi executeUpdate ko tu dong luu xuong database nua
-                    connect.setAutoCommit(true);
-            }catch (SQLException e){
-                e.printStackTrace();
-            }
-        }
-        return 0;//that bai insert
+            throw new RuntimeException("Update failed");
+         }finally{
+             try{
+                 if (connect != null)
+                     connect.setAutoCommit(true);
+             }catch(Exception e){
+                 e.printStackTrace();
+             }
+         }
+     }
 
-    }
-    // return tra ve so co nghai la so dong thay doi thanh cong, tra ve 0 nghia la that bai
-    //update khong duoc sua gia bat dau(starting_price)
-    public int update(Item item){
-        String sql="UPDATE items SET name=?, description=? WHERE id=?";
-        try(Connection connect=DatabaseConnection.getConnection();
-            PreparedStatement ps=connect.prepareStatement(sql)){
-            ps.setString(1, item.getName());
-            ps.setString(2,item.getDescription());
-            ps.setInt(3, parseInt(item.getId()));
-            return ps.executeUpdate();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return 0;
-    }
-    //delete item
-    public int delete(Item item){
-        String sql="DELETE FROM items WHERE id=?";
-        try(Connection connect=DatabaseConnection.getConnection();
-          PreparedStatement ps=connect.prepareStatement(sql)){
-            ps.setInt(1, parseInt(item.getId()));
-            return ps.executeUpdate();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return 0;
-    }
-    //tim bang id
-    public Item selectById(String id){
-        String sql="SELECT * FROM items WHERE id=?";
-        try(Connection connect=DatabaseConnection.getConnection();
-            PreparedStatement ps=connect.prepareStatement(sql)){
-            ps.setInt(1, Integer.parseInt(id));
-            ResultSet resultSet=ps.executeQuery();
-            //tim lan luot, lay id va type, so sanh type xem vao truong hop nao
-            if(resultSet.next()){
-                int itemId=resultSet.getInt("id");
-                String type=resultSet.getString("item_type");
-                if("VEHICLE".equals(type)){
-                    PreparedStatement ps1=connect.prepareStatement("SELECT * FROM vehicle_items WHERE item_id=?");
-                    ps1.setInt(1,itemId);
-                    ResultSet rs2=ps1.executeQuery();
-                    if(rs2.next()){
-                        return new Vehicle(id,resultSet.getString("name"),
-                                   resultSet.getString("description"),
-                                   resultSet.getDouble("starting_price"),
-                                   rs2.getInt("year"),
-                                   rs2.getDouble("mileage")
-                        );
-                    }
+     public int delete(int id) {
+         try (Connection connect = new DatabaseConnection().getConnection()) {
+             String sql = "DELETE FROM items WHERE id =?";
+             PreparedStatement ps = connect.prepareStatement(sql);
+             ps.setInt(1, id);
+             //return lai so dong bi xoa
+             return ps.executeUpdate();
+         } catch (Exception e) {
+             throw new RuntimeException("Delete failed");
+         }
+     }
+
+
+     public Item selectById(int id) throws SQLException {
+         try(Connection connect =new DatabaseConnection().getConnection()){
+             String sql ="SELECT * FROM items WHERE id =?";
+             PreparedStatement ps= connect.prepareStatement(sql);
+             ps.setInt(1,id);
+             ResultSet rs=ps.executeQuery();
+             if (!rs.next())
+                 return null;
+             String type=rs.getString("item_type");
+             String name =rs.getString("name");
+             String description=rs.getString("description");
+             double starting_price=rs.getDouble("starting_price");
+             int itemId=rs.getInt("id");
+             if ("VEHICLE".equals(type)){
+                 PreparedStatement ps2=connect.prepareStatement("SELECT * FROM vehicle_items WHERE item_id=?");
+                 ps2.setInt(1, itemId);
+                 ResultSet rs2=ps2.executeQuery();
+                 if (rs2.next()){
+                     Vehicle vehicle=new Vehicle(name, description, starting_price, rs2.getInt("year"), rs2.getDouble("mileage"));
+                     vehicle.setId(itemId);
+                     return vehicle;
+                 }
+             }
+            else if("ART".equals(type)){
+                PreparedStatement ps2=connect.prepareStatement("SELECT * FROM art_items WHERE item_id=?");
+                ps2.setInt(1,itemId);
+                ResultSet rs2=ps2.executeQuery();
+                if(rs2.next()){
+                    Art art=new Art(name, description, starting_price, rs2.getString("artist"), rs2.getString("theme"), rs2.getString("material"));
+                    art.setId(itemId);
+                    return art;
                 }
-                else if ("ART".equals(type)) {
-                    PreparedStatement ps2 = connect.prepareStatement("SELECT * FROM art_items WHERE item_id=?");
-                    ps2.setInt(1, itemId);
-                    ResultSet rs2 = ps2.executeQuery();
-                    if (rs2.next()) {
-                        return new Art(
-                                id,
-                                resultSet.getString("name"),
-                                resultSet.getString("description"),
-                                resultSet.getDouble("starting_price"),
-                                rs2.getString("artist"),
-                                rs2.getString("theme"),
-                                rs2.getString("material")
-                        );
-                    }
+             }
+            else if("ELECTRONICS".equals(type)){
+                PreparedStatement ps2=connect.prepareStatement("SELECT * FROM electronics_items WHERE item_id=?");
+                ps2.setInt(1,itemId);
+                ResultSet rs2=ps2.executeQuery();
+                if (rs2.next()){
+                    Electronics electronic=new Electronics(name,description,starting_price, rs2.getString("brand"),rs2.getInt("warranty_months") );
+                    electronic.setId(itemId);
+                    return electronic;
                 }
-                else if ("ELECTRONICS".equals(type)) {
-                    PreparedStatement ps3 = connect.prepareStatement("SELECT * FROM electronics_items WHERE item_id=?");
-                    ps3.setInt(1, itemId);
-                    ResultSet rs3 = ps3.executeQuery();
-                    if (rs3.next()) {
-                        return new Electronics(
-                                id,
-                                resultSet.getString("name"),
-                                resultSet.getString("description"),
-                                resultSet.getDouble("starting_price"),
-                                rs3.getString("brand"),
-                                rs3.getInt("warranty_months")
-                        );
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-}
+             }
+            return null;
+         }
+     }
+ }
