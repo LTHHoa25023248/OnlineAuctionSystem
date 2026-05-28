1f3e43912bc0d2c09c553d0f010b2624686ed987
Phạm Đức Huy <25023270@vnu.edu.vn>
2026-05-28 16:13:32 +0700

    Remove 'RUNNING' status from AuctionStatus enum

--- src/main/java/com/example/auctionmanagementsystem/model/AuctionStatus.java
+++ src/main/java/com/example/auctionmanagementsystem/model/AuctionStatus.java
@@ -2,6 +2,6 @@ package com.example.auctionmanagementsystem.model;
 
 // Enumeration-const
 public enum AuctionStatus {
-  OPEN, RUNNING, FINISHED, PAID, CANCELED, PENDING, REJECTED
+  OPEN, FINISHED, PAID, CANCELED, PENDING, REJECTED
 }
 
