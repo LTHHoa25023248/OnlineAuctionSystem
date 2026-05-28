8c17ec31d641cd9ca8a7ff22c38e3faf1007538f
Phạm Đức Huy <25023270@vnu.edu.vn>
2026-05-28 21:28:08 +0700

    Delete src/main/resources/com/example/database directory

--- src/main/resources/com/example/database/art_items.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE art_items (
-    item_id INT PRIMARY KEY,
-    artist VARCHAR(255),
-    theme VARCHAR(255),
-    material VARCHAR(255),
-
-    FOREIGN KEY (item_id) REFERENCES items(id)
-    ON DELETE CASCADE
-);
--- src/main/resources/com/example/database/vehicle_items.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE vehicle_items (
-    item_id INT PRIMARY KEY,
-    year INT,
-    mileage DOUBLE,
-
-    FOREIGN KEY (item_id) REFERENCES items(id)
-    ON DELETE CASCADE
-);
