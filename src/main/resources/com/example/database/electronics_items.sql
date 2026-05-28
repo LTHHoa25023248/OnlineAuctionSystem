c526daf74f8d9d12d4cbfb90db2021ff101cb609
Pham Duc Huy <25023270@vnu.edu.vn>
2026-05-28 21:31:33 +0700

    them database chuan

--- src/main/resources/com/example/database/electronics_items.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE electronics_items (
-    item_id INT PRIMARY KEY,
-    brand VARCHAR(255),
-    warranty_months INT,
-
-    FOREIGN KEY (item_id) REFERENCES items(id)
-    ON DELETE CASCADE
-);
