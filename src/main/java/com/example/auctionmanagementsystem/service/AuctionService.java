d059fbaad7be7b9d69c39d56c33e70d74836b288
LTHHoa_25023248 <25023248@vnu.edu.vn>
2026-05-31 16:38:41 +0700

    Update AuctionService.java

--- src/main/java/com/example/auctionmanagementsystem/service/AuctionService.java
+++ src/main/java/com/example/auctionmanagementsystem/service/AuctionService.java
@@ -44,7 +44,7 @@ public class AuctionService {
                 throw new IllegalStateException("Auction must be PENDING to start");
             }
             //set trang thai, time, update data
-            auction.setStatus(AuctionStatus.OPEN);
+            auction.setStatus(AuctionStatus.RUNNING);
             auction.setStartTime(LocalDateTime.now());
             auctionDao.update(auction, connect);
 
